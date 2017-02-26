19a5a64b8f28de4502af80657bad33ab3bd4ab9e
Michal Miedlarz <m.miedlarz@nomtek.com>
2017-02-26 12:05:00 +0100

    Added Quick and Nimble pods

---
+++ Comrade/Comrade/Sources/Core/Comrade.swift
@@ -0,0 +1,17 @@
+//
+//  Comrade.swift
+//  Comrade
+//
+//  Created by Michal Miedlarz on 26.02.2017.
+//  Copyright © 2017 DogsField. All rights reserved.
+//
+
+import Foundation
+
+class Comrade
+{
+    init()
+    {
+        
+    }
+}
