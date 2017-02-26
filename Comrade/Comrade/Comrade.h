69a1b8e6d1d283567bb7755cbb3d16ae80c842a0
Michal Miedlarz <m.miedlarz@nomtek.com>
2017-02-26 11:48:46 +0100

    Initial commit

---
+++ Comrade/Comrade/Comrade.h
@@ -0,0 +1,19 @@
+//
+//  Comrade.h
+//  Comrade
+//
+//  Created by Michal Miedlarz on 26.02.2017.
+//  Copyright © 2017 DogsField. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Comrade.
+FOUNDATION_EXPORT double ComradeVersionNumber;
+
+//! Project version string for Comrade.
+FOUNDATION_EXPORT const unsigned char ComradeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Comrade/PublicHeader.h>
+
+
